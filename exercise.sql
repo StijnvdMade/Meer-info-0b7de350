75ec3544c339343b4e5b56890ea6af329dd17303
Stijn van_der_Made <2120804@talnet.nl>
2020-01-23 13:35:34 +0100

    Uitwerking van de exercise

---
+++ exercise.sql
@@ -0,0 +1,21 @@
+-- CREATE DATABASE Sterrenstelsel;
+USE sterrenstelsel;
+-- CREATE TABLE Planeten (name VARCHAR(20));
+-- INSERT INTO planeten (name) VALUES('Mercurius');
+-- INSERT INTO planeten (name) VALUES('Aarde');
+-- INSERT INTO planeten (name) VALUES('Mars');
+SHOW TABLES;
+-- describe planeten;
+-- truncate planeten;	
+-- CREATE TABLE `informatie` (
+-- `naam` varchar(15), 
+-- `diameter` int(10), 
+-- `afstand tot de zon` int(15),
+-- `massa tov de aarde` int(10)
+-- );
+
+-- INSERT INTO informatie (naam, diameter, `afstand tot de zon`, `massa tov de aarde`) VALUES ('Venus', 12104, 108208930, 0.9);
+-- INSERT INTO informatie (naam, diameter, `afstand tot de zon`, `massa tov de aarde`) VALUES ('Jupiter', 142.984, 778412010, 318);
+-- INSERT INTO informatie (naam, diameter, `afstand tot de zon`, `massa tov de aarde`) VALUES ('Uranus', 51118, 287097220, 15);
+describe informatie;
+
